ee1dc37663ca451a93a807797a18b5b61e307905
Klevion Xhoxhaj <klevionxhoxhaj@gmail.com>
2024-04-07 23:40:15 +0200

    -mColor Change Sample

---
+++ src/Input.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import colorNames from 'colornames';
+const Input = ({colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText}) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className='searchInput'>
+      <label>Add Color Name:</label>
+      <input
+        autoFocus
+        type='text'
+        placeholder='Add color name'
+        required
+        value={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+          setHexValue(colorNames(e.target.value))
+        }}
+      />
+      <button
+        type='button'
+        onClick={() => setIsDarkText(!isDarkText)}
+      >
+        Toggle Text Color
+      </button>
+    </form>
+  )
+}
+
+export default Input
